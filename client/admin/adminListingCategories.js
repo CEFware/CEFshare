5c33492c0e4b09483c11e5fd0d121d2eb775ac5b
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-05-05 14:33:50 +0300

    admin categories edit finished

--- client/admin/adminListingCategories.js
+++ client/admin/adminListingCategories.js
@@ -11,6 +11,34 @@ Template.adminListingCategories.helpers({
         var res=Categories.find({parent:this.name}).fetch();
         if (res.length>0)
             return res;
+    },
+    countArr: function (arr) {
+	if (arr)
+	    return arr.length();
+	return 0;
+    },
+    letDelete: function (catObj) {
+	if (catObj.parent) {
+	    //children category
+	    if (catObj.listings && (catObj.listings.length()>0))
+		    return false;
+	    return true;
+	} else {
+	    //parent category
+	    if (catObj.listings && (catObj.listings.length()>0)) {
+		    return false;
+	    } else {
+		//check if no sub categories
+		if (Categories.find({parent:catObj.name}).count()>0)
+		    return false;
+		return true;
+	    };
+	};
+    },
+    editingCategory: function (id) {
+	if (Session.get('editingCategory')===id)
+	    return true;
+	return false;
     }
 });
 
@@ -24,3 +52,32 @@ Template.adminListingCategories.rendered = function () {
     Meteor.subscribe('categories');
 };
 
+Template.adminListingCategories.events({
+    'click .adminListingCategoryDelete': function (e,t) {
+	e.preventDefault();
+	Meteor.call('deleteCategory',this._id);
+    },
+    'click .fa-pencil': function (e,t) {
+	e.preventDefault();
+	Session.set('editingCategory',this._id);
+    }
+});
+
+Template.editCategoryName.events({
+    'click .stopEdit': function (e,t) {
+	e.preventDefault();
+	Session.set('editingCategory',null);
+    },
+    'click .saveEdit': function (e,t) {
+	e.preventDefault();
+	var newName=t.$('#'+this._id).val();
+	Meteor.call('updateCategory',this._id,newName,function (e) {
+	    if (!e) 
+		Session.set('editingCategory',this._id);
+	});
+    }
+});
+
+Template.editCategoryName.rendered = function () {
+    $('.newCatName').focus();
+};
--- server/categories/methods.js
+++ server/categories/methods.js
@@ -9,5 +9,19 @@ console.log(l);
 console.log(cat);
             //add listing to new category
 	};
+    },
+    deleteCategory: function (id) {
+	if (Roles.userIsInRole(Meteor.userId(),'admin')) {
+	    Categories.remove({_id:id});
+	};
+    },
+    updateCategory: function (id, newName) {
+	if (Roles.userIsInRole(Meteor.userId(),'admin')) {
+	    var res=Categories.findOne({_id:id,parent:{$exists:false}});
+	    if (res)
+		//if parent - change all children categories parent field
+		Categories.update({parent:res.name},{$set:{parent:newName}},{multi:true});
+	    Categories.update({_id:id},{$set:{name:newName}});
+	};
     }
 });
