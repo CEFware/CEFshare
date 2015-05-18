2016a1e0fd0dae8427d3656a6774de60935bf783
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-05-18 17:12:07 +0300

    new way to change theme color & take this data from admin

--- client/layout/head.js
+++ client/layout/head.js
@@ -1,11 +1,12 @@
 Meteor.startup(function() {
     if (Meteor.settings) {
 	document.title=Meteor.settings.public.marketplaceName;
-	var sc = document.createElement("link");
+/*	var sc = document.createElement("link");
 	sc.setAttribute("rel", "stylesheet");
 	sc.setAttribute("id", "theme-stylesheet");
 	sc.setAttribute("type", "text/css");
 	sc.setAttribute("href", "/css/style."+Meteor.settings.public.color+".css");
 	document.head.appendChild(sc);
+*/
     };
 });
