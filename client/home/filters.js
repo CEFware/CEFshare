1f55df8efadb70ed371399fd372bf395752cc945
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-05-22 18:09:12 +0300

    filters: show filters according to admin setting

--- client/home/filters.js
+++ client/home/filters.js
@@ -1,3 +1,38 @@
+var RangeSchema = new SimpleSchema({
+    lower: {
+	type: Number
+    },
+    upper: {
+	type: Number
+    }
+});
+
+var baseFilters = new SimpleSchema ({
+    range: {
+	type: RangeSchema,
+	max: 100,
+	min: 0,
+	optional: true,
+	autoform: {
+	    type: "noUiSlider"
+	}
+    },
+    bool: {
+	type: [String],
+	optional: true,
+	autoform: {
+	    options: [{label:'YES',value:true}, {label:'NO',value:false}]
+	}
+    },
+    options: {
+	type: [String],
+	optional: true,
+	autoform: {
+	    options: [{label:'first',value:'f'}, {label:'2nd',value:'2'}]
+	}
+    }
+});
+
 Template.filters.helpers({
     parentCategories: function () {
 	return Categories.find({parent:{$exists:false}},{sort:{name:1}});
@@ -33,7 +68,43 @@ Template.filters.helpers({
     },
     homeFiltersSchemaObj: function () {
 	//build custom SimpleSchema for the filters form our of base schema
-	return new SimpleSchema({});
+	var res=Main.findOne();
+	var schema=[];
+	if (res && res.filters) {
+	    res.filters.forEach(function (el){
+		if (el.active) {
+		    var type=Listing._schema[el.fieldName].type.name;
+		    switch (type) {
+		    case 'String':
+			var nObj = {};
+			nObj[el.fieldName]=baseFilters._schema.options;
+			nObj[el.fieldName+'.$']=baseFilters._schema['options.$'];
+			schema.push(nObj);
+ 			break;
+		    case 'Number':
+			var nObj = {};
+			nObj[el.fieldName]=baseFilters._schema.range;
+			schema.push(nObj);
+			break;
+		    case 'Boolean':
+			var nObj = {};
+			nObj[el.fieldName]=baseFilters._schema.bool;
+			nObj[el.fieldName+'.$']=baseFilters._schema['bool.$'];
+			schema.push(nObj);
+			break;
+		    default:
+			break;
+		    };
+		};
+	    });
+	    var finSchema=new SimpleSchema(schema);
+	    var objL={};
+	    res.filters.forEach(function (el){
+		objL[el.fieldName]=el.title;
+	    });
+	    finSchema.labels(objL);
+	};
+	return finSchema;
     },
     homeFiltersNow: function () {
 	//return current stage of the filters
--- server/admin/publish.js
+++ server/admin/publish.js
@@ -17,10 +17,12 @@ Meteor.publish('appSettings', function(){
 });
 
 Meteor.publish('typesCount', function () {
-    Main.findOne().listingFields.forEach( function (el) {
-	var res=Listings.find({listingType:el.listingType}).count();
-	TypesCount.update({listingType:el.listingType}, {$set: {listingType:el.listingType, amount: res}}, {upsert:true});
-    });
+    var frt=Main.findOne().listingFields;
+    if (frt)
+	frt.forEach( function (el) {
+	    var res=Listings.find({listingType:el.listingType}).count();
+	    TypesCount.update({listingType:el.listingType}, {$set: {listingType:el.listingType, amount: res}}, {upsert:true});
+	});
     return TypesCount.find();
 });
 
