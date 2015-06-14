47721272420848fd81979b6c0aa52ebf341e4824
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-06-14 23:17:20 +0300

    list of paid plans to select from in admin main page

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -13,10 +13,32 @@ Template.admin.helpers({
 	if (ServiceConfiguration.configurations.find({service:{$ne:"stripe"}}).count()>0)
 	    return 'success';
 	return 'danger';
+    },
+    equals: function(a,b) {
+	return (a==b);
+    },
+    curPlan: function () {
+	return Session.get('curPlan');
+    }
+});
+
+Template.admin.events({
+    'click .monthly': function (e) {
+	e.preventDefault();
+	Session.set('curPlan', 'monthly');
+    },
+    'click .halfYear': function (e) {
+	e.preventDefault();
+	Session.set('curPlan', 'halfYear');
+    },
+    'click .yearly': function (e) {
+	e.preventDefault();
+	Session.set('curPlan', 'yearly');
     }
 });
 
 Template.admin.rendered=function () {
+    Session.set('curPlan', 'halfYear');
     $('#timeleft').countdown((new Date()).setDate((new Date(Main.findOne().createdAt)).getDate()+30), function(event) {
 	$(this).html(event.strftime('%D days %H:%M:%S'));
     });
