1fd8846ddb422b453347c3d4e64f4483cde20325
Piyush Thapa <piyush.coader@gmail.com>
2015-06-24 15:46:52 +0545

    sidebar active menu created

---
+++ client/pages/includes/sideBarAdmin.js
@@ -0,0 +1,12 @@
+/**
+ * Created by piyushthapa on 6/24/15.
+ */
+Template.sidebar_admin_desktop.events({
+   'click .main-sidebar li a':function(e){
+       e.preventDefault();
+       var href=$(e.target).attr('href');
+       $('.main-sidebar li').removeClass('active-sidebar');
+       $(e.target).parent().addClass('active-sidebar');
+       Router.go(href);
+   }
+});
