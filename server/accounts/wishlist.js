287ed1ef0fa33fa91b49667bf2d47b5208e4b9c9
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-06-30 16:20:29 +0300

    make wislist work - On user profile page - click of wishlist card takes you to dead route #36

--- server/accounts/wishlist.js
+++ server/accounts/wishlist.js
@@ -1,15 +1,1 @@
-Meteor.publish('wishlist', function (username) {
-    if (username) {
-	var res=Meteor.users.findOne({username:username});
-	if (res) {
-	    return Wishlist.find({
-		user_id:res._id
-	    });
-	};
-    } else {
-	return Wishlist.find({
-	    user_id: this.userId
-	});
-    };
-});
-
+Meteor.publish('wishlist', getWishlist);
