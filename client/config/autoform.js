6052da8ffe235a869f103742f226801616432da7
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-07-01 12:40:46 +0300

    CMS - the content management functions need to be remodeled so they are fully data driven #50 - 75% done

--- client/config/autoform.js
+++ client/config/autoform.js
@@ -2,5 +2,5 @@
  * Created by piyushthapa on 6/19/15.
  */
 AutoForm.setDefaultTemplate('materialize');
-AutoForm.setDefaultTemplateForType('afArrayField','menuLInks');
+//AutoForm.setDefaultTemplateForType('afArrayField','menuLInks');
 //AutoForm.setDefaultTemplateForType('afFileUpload','custom');
--- client/pages/admin/adminLinks.js
+++ client/pages/admin/adminLinks.js
@@ -1,13 +1,29 @@
 Template.adminLinks.helpers({
-    linksSchemaObj: function() {
-        return linksSchema;
+    pagesSchemaObj: function() {
+        return pagesSchema;
     },
-    mainLinks: function() {
-        var res=Main.findOne();
+    pages: function() {
+        var res=Pages.find();
         if (res)
-            return res.menuLinks;
+            return res;
+    },
+    fType: function () {
+        var resId=Session.get('pageId');
+        if (resId)
+            return "update";
+        return "insert";
+    },
+    curPage: function () {
+        var resId=Session.get('pageId');
+        if (resId) {
+            var resPage=Pages.findOne({_id:resId});
+            if (resPage) {
+                return resPage;
+            };
+        };
+        return false;
     }
- });
+});
 
 AutoForm.addHooks(['adminLinks'],{
     onSuccess: function (){
@@ -19,9 +35,26 @@ Template.adminLinks.rendered = function () {
     Meteor.subscribe('appSettings');
     $('.ck-editor').ckeditor();
     $('.ck-editor').parent().children('label').remove();
+    $('.ck-editor').parent().prepend('<h6>Page text</h6>');
 };
 
-Template.afQuickField.onRendered(function(){
-   var card=$('.card-panel');
-  card.removeClass('card-panel');
+Template.adminLinks.events({
+    'click .delPage': function (e,t) {
+        e.preventDefault();
+        if (Session.get('pageId')===this._id) {
+            $('[name=title]').val("");
+            Session.set('pageId',null);
+        };
+        Meteor.call('deletePage',this._id);
+    },
+    'click .cancel': function (e,t) {
+        e.preventDefault();
+        $('[name=title]').val("");
+	$('.ck-editor').val("");
+        Session.set('pageId',null);
+    },
+    'click .editPage': function (e,t) {
+        e.preventDefault();
+        Session.set('pageId',this._id);
+    }
 });
