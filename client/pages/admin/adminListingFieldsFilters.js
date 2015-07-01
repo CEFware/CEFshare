d4edaaca0caa7d442b592d2e1ea106acfc10a2a2
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-07-01 23:42:52 +0300

    work on #19

--- client/pages/admin/adminListingFieldsFilters.js
+++ client/pages/admin/adminListingFieldsFilters.js
@@ -27,24 +27,6 @@ Template.adminListingFieldsFilters.helpers({
 	if (Main.findOne({'defaultListingFields.listingType':this.listingType}))
             return false;
         return true;
-    },
-    fieldName: function () {
-        return Router.current().params.field;
-    },
-    oneFilterObj: function () {
-        return oneFilter;
-    },
-    fieldTitle: function () {
-        if (Router.current().params.field === 'listingType')
-            return ListingDefault._schema.listingType.label();
-        return _.filter(_.filter(Main.findOne().listingFields,function (el) {
-            return el.listingType=== Router.current().params.listingType})[0].listingFields, function (el2) {return el2.type===Router.current().params.field})[0].title;
-    },
-    filterObj: function () {
-        res=_.filter(Main.findOne().filters, function (el) {return el.fieldName===Router.current().params.field})[0];
-        if (res)
-            return res;
-        return {title:'',active:false,fieldName:Router.current().params.field};
     }
 });
 
