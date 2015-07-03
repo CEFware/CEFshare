1f2d88a82ffb2b33b9a2ce3421f430cfa102706e
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-07-03 12:08:37 +0300

    social icons in side menu data driven by admin-social

--- client/pages/includes/sidebar_main_Desktop.js
+++ client/pages/includes/sidebar_main_Desktop.js
@@ -24,5 +24,29 @@ Template.sidebar_main_Desktop.helpers({
     },
     pages: function () {
 	return Pages.find();
+    },
+    fb:function () {
+	var res=Main.findOne();
+	if (res && res.socialAccounts && res.socialAccounts.fbHandle)
+	        return res.socialAccounts.fbHandle;
+	return false;
+    },
+    tw:function () {
+	var res=Main.findOne();
+	if (res && res.socialAccounts && res.socialAccounts.twitterHandle)
+	        return res.socialAccounts.twitterHandle;
+	return false;
+    },
+    google:function () {
+	var res=Main.findOne();
+	if (res && res.socialAccounts && res.socialAccounts.googleHandle)
+	        return res.socialAccounts.googleHandle;
+	return false;
+    },
+    social: function () {
+	var res=Main.findOne();
+	if (res && res.socialAccounts && (res.socialAccounts.fbHandle || res.socialAccounts.twitterHandle || res.socialAccounts.googleHandle))
+	        return true;
+	return false;
     }
 });
