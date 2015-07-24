77e7904a5fd4af5471452aeab77a275cc08b386e
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-07-24 15:07:22 +0300

    new port for cefshare.com

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # Script for starting the app during development.
 
-ROOT_URL='http://cefshare.com/' MAIL_URL='smtp://meteor.test.mailbox%40gmail.com:meteor.test@smtp.gmail.com:465' meteor --settings private/settings.json -p 80
+ROOT_URL='http://cefshare.com/' MAIL_URL='smtp://meteor.test.mailbox%40gmail.com:meteor.test@smtp.gmail.com:465' meteor --settings private/settings.json -p 3000
